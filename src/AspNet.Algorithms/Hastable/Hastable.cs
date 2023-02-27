775ba36a1028a53a21db0c23f07befbee1cd0dd9
Fernando Gonçalves <hd1.fernandosouza@hotmail.com>
2023-02-26 21:17:21 -0300

    feat: add a dumb solution to add and get a value in a hashtable

---
+++ src/AspNet.Algorithms/Hastable/Hastable.cs
@@ -0,0 +1,33 @@
+﻿namespace AspNet.Algorithms;
+
+public class Hastable<TKey, TValue> where TKey : IComparable<TKey>
+{
+    readonly KeyValuePair<TKey, TValue>[] _data;
+
+    public Hastable(int capacity)
+    {
+        _data = new KeyValuePair<TKey, TValue>[capacity];
+    }
+
+    public void Add(TKey key, TValue value)
+    {
+        var hash = key.GetHashCode();
+        var position = hash % _data.Length;
+        _data[position] = new KeyValuePair<TKey, TValue>(key, value);
+    }
+
+
+    public TValue Get(TKey key)
+    {
+        var hash = key.GetHashCode();
+        var position = hash % _data.Length;
+
+        if (_data[position] is null)
+            throw new KeyNotFoundException();
+
+        return _data[position].Value;
+    }
+}
+
+public record KeyValuePair<TKey, TValue>(TKey key, TValue Value);
+
---
+++ test/AspNet.Algorithms.Test/Hastable/HastableTests.cs
@@ -0,0 +1,20 @@
+﻿using AspNet.Algorithms;
+using FluentAssertions;
+
+namespace AspNet.Algorithms.Test;
+
+public class HastableTests
+{
+
+    [Fact(DisplayName = "Get the exactly value passed on the key")]
+    public void TestOne()
+    {
+        var hash = new Hastable<int, int>(1);
+
+        hash.Add(1,1);
+
+        var result = hash.Get(1);
+
+        result.Should().Be(1);
+    }
+}
